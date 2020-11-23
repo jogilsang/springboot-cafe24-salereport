87de6ce2deb9449e2ed5d143b9a365df15b965ea
jogilsang <jogilsang@gmail.com>
2020-11-24 01:14:55 +0900

    init

---
+++ main/java/com/cafe24/gilsang/app/core/AppEnv.java
@@ -0,0 +1,16 @@
+package com.cafe24.app.sample.core;
+
+public class AppEnv {
+
+    //App key
+    public static final String CLIENT_ID = "mcZ0kV6eOa1LjmsS37eruP";//<-------------------------------- [App key] 수정
+    //App secret key
+    public static final String SECRET_KEY = "wHd456MNFdQkhnrz0UfDwA";//<-------------------------------- [Secre key] 수정
+    //app Url
+    public static final String APP_BASE_URL = "https://jogilsang.xyz:7000";//<-------------------------------- [App host] 수정
+    //app retrun Url
+    public static final String APP_RETURN_URL = "https://jogilsang.xyz:7000/admin";//<-------------------------------- [App return url] 수정
+    //app scope
+    public static final String APP_SCOPE = "mall.read_salesreport";//<-------------------------------- [App scope] 수정
+
+}
